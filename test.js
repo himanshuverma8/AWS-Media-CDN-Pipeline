22ebaf502a3de6d9a7cf04a773e9b9f4d1fd0200
himanshuverma8 <hvinprimary@gmail.com>
2025-12-13 19:08:19 +0530

    add test file for debugging

---
+++ test.js
@@ -0,0 +1,13 @@
+// const prefix = 'format=jpeg,height=100,width=200'
+// console.log(`this is the original string: ${prefix}`);
+// const arrOfPrefix = prefix.split(',');
+// console.log(arrOfPrefix);
+// const newarrOfPrefix = arrOfPrefix.map((item) => {
+//     return item.split('=');
+// });
+// console.log(newarrOfPrefix);
+// const keyValPairsObject = Object.fromEntries(
+//     newarrOfPrefix
+//     )
+
+//     console.log(Object.entries(keyValPairsObject));
